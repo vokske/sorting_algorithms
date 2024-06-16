7b0d12c5c1603c11d9697c7b8ebb1ac6708efc75
vokske <kevinmuigai2@gmail.com>
2024-06-16 13:38:32 +0300

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,15 +3,14 @@
 void bubble_sort(int *array, size_t size)
 {
 	long unsigned int i, j;
-	int temp;
+	int swap_flag, temp;
 
 	if (size < 2)
 		return;
 
 	for (i = 0; i < size; i++)
 	{
-		/*swap_flag = 0;
-		 */
+		swap_flag = 0;
 
 		for (j = 0; j < size - i - 1; j++)
 		{
@@ -20,13 +19,12 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-		/*		swap_flag = 1;
-		 */
+				swap_flag = 1;
 			}
+			print_array(array, size);
 		}
-		/*if (swap_flag == 0)
-		*	break;
-		*/
-		print_array(array, size);
+		if (swap_flag == 0)
+			break;
 	}
+	print_array(array, size);
 }
