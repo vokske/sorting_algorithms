83ab11b3f19d6d3192bbdbbbebad327aa2a198b7
vokske <kevinmuigai2@gmail.com>
2024-06-17 17:04:12 +0300

    Insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp, *current, *next_node;
+
+	if (*list == NULL || (*list)->next == NULL)
+		return;
+
+	current = (*list)->next;
+
+	while (current != NULL)
+	{
+		next_node = current->next;
+
+		while (current->prev != NULL && current->n < current->prev->n)
+		{
+			temp = current->prev;
+
+			if (temp->prev != NULL)
+				temp->prev->next = current;
+			
+			current->prev = temp->prev;
+			temp->next = current->next;
+
+			if (current->next != NULL)
+				current->next->prev = temp;
+
+			current->next = temp;
+			temp->prev = current;
+
+			if (current->prev == NULL)
+				*list = current;
+
+			print_list(*list);
+		}
+		current = next_node;
+	}
+}
